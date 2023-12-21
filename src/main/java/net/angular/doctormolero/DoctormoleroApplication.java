e5d1546d79e441ad3b2f8a0d038c81aac12b7a02
PabloMolero <pmoleromarin@gmail.com>
2023-12-21 10:19:33 +0100

    subida inicial

---
+++ src/main/java/net/angular/doctormolero/DoctormoleroApplication.java
@@ -0,0 +1,13 @@
+package net.angular.doctormolero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctormoleroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctormoleroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/angular/doctormolero/DoctormoleroApplicationTests.java
@@ -0,0 +1,13 @@
+package net.angular.doctormolero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctormoleroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
